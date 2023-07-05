9660536edee7f1f08aa6dacdcc6732b13659cc13
OpenAI <OpenAI@CHATGPT>
2023-07-05 15:19:26 +0500

    Add project files.

---
+++ SampleDLL/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleDLL
+{
+    public class Class1
+    {
+        // int n1, n2;
+
+        public Class1() { }
+        public int addNumbers(int n1, int n2) {
+            int sum1 = n1 + n2;
+            return sum1;
+
+        }
+    }
+}
